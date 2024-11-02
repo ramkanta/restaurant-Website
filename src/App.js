2b7f6154a9f8b4ead596e138e4e3a8e3f82ede0b
surokha <ramkantapramanik9679@gmail.com>
2024-11-02 07:57:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './pages/Home';
+import Menu from './pages/Menu'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Pagenotfound from './pages/Pagenotfound'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/contact' element={<Contact></Contact>}></Route>
+      <Route path='/menu' element={<Menu></Menu>}></Route>
+      <Route path='*' element={<Pagenotfound></Pagenotfound>}></Route>
+    </Routes>
+   </BrowserRouter>
+
+   </>
   );
 }
 
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import { Drawer } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import { Link } from 'react-router-dom';
+import '../../styles/HeaderStyle.css';
+
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <FastfoodIcon /> Fast Food
+      </Typography>
+
+      <ul className="mobile-navigation">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+      </ul>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2, display: { sm: 'none' } }}
+            onClick={handleDrawerToggle} // Added onClick to toggle drawer
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <FastfoodIcon /> Fast Food
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <ul className="navigation-menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/menu">Menu</Link>
+              </li>
+            </ul>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: 240,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
